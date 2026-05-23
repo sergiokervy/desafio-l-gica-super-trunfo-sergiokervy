ca4969fa2a8f4677f781ca97c16451e653e2451b
sergiokervy <sergiokervy@gmail.com>
2026-05-23 19:48:16 +0000

    limitação do valor da densidade para dois digitos

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -4,33 +4,34 @@ int main() {
    int populacao1, populacao2;
    char carta1[20], carta2[20];
    float area1, area2, densidade1, densidade2;
-    
+
+   printf("     Bem-vindo ao jogo de cartas Super Trunfo - Densidade Populacional!\n");
    printf("digite o nome da carta 1:\n");
     scanf("%s", &carta1);
     printf("agora digite seu número populacional:\n");
     scanf("%d", &populacao1);
-    printf("por fim, digite sua área:\n");
+    printf("por fim, digite o valor de sua área:\n");
     scanf("%f", &area1);
     scanf("carta 1 registrada!\n");
    printf("digite o nome da carta 2:\n");
     scanf("%s", &carta2);
     printf("agora digite sua população:\n");
     scanf("%d", &populacao2);
-    printf("por fim, digite sua área:\n");
+    printf("por fim, digite o valor de sua área:\n");
     scanf("%f", &area2);
     scanf("carta 2 registrada!\n");
 
     densidade1 = populacao1 / area1;
     densidade2 = populacao2 / area2;
 
-    printf("resultado do calculo da densidade populacional:\n");
-    printf("carta 1 = %d/%f=%f - carta 2 = %d/%f=%f\n", populacao1, area1, densidade1, populacao2, area2, densidade2);
+    printf("\n          RESULTADO DO CÁLCULO DA DENSIDADE POPULACIONAL:\n");
+    printf("%s= %d/%.2f = [%.2fhab/km²]   |   %s= %d/%.2f = [%.2fhab/km²]\n", carta1, populacao1, area1, densidade1, carta2, populacao2, area2, densidade2);
 
     if(densidade1 < densidade2){
-        printf("a carta 1 (%s) possui menor densidade populacional, logo é a vencedora!\n", carta1);
+        printf("\na CARTA 1 (%s) possui menor densidade populacional, logo é a vencedora!\n\n", carta1);
     }
     else{
-        printf("a carta 2 (%s) possui menor densidade populacional, logo é a vencedora!\n", carta2);
+        printf("\na CARTA 2 (%s) possui menor densidade populacional, logo é a vencedora!\n\n", carta2);
         }
 
 
